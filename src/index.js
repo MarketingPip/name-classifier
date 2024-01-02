b53d64837d7c5a3662b57edfdf4c7665e1036431
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-01-02 09:18:16 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// femaleNames.js
+import femaleNames from './femaleNames.json';
+
+// femaleNicknames.js
+import femaleNicknames from './femaleNicknames.json';
+
+// maleNames.js
+import maleNames from './maleNames.json';
+
+// maleNicknames.js
+import maleNicknames from './maleNicknames.json';
+
+// Exporting all the data
+export {
+  femaleNames,
+  femaleNicknames,
+  maleNames,
+  maleNicknames,
+};
