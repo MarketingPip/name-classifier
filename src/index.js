437ffb77d4c11cfa8cc6165592a3b4403ef56ba1
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-01-02 09:38:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // femaleNames.js
-import * as femaleNames from './femaleNames.json';
+import femaleNames from './femaleNames.json' assert { type: `json` };
 
 // femaleNicknames.js
-import * as femaleNicknames from './femaleNicknames.json';
+import femaleNicknames from './femaleNicknames.json' assert { type: `json` };
 
 // maleNames.js
-import * as maleNames from './maleNames.json';
+import maleNames from './maleNames.json' assert { type: `json` };
 
 // maleNicknames.js
-import * as maleNicknames from './maleNicknames.json';
+import maleNicknames from './maleNicknames.json' assert { type: `json` };
 
 // Exporting all the data
 export {
