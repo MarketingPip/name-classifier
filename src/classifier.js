41c9c9be245e17e4e907db9e39e43e7d3da67d27
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-01-03 04:20:27 -0500

    Create classifier.js

---
+++ src/classifier.js
@@ -0,0 +1,36 @@
+const { NlpManager } = require('node-nlp');
+const fs = require('fs').promises;
+
+const manager = new NlpManager({ languages: ['en'] });
+
+
+(async () => {
+  const nlp = manager;
+  nlp.settings.autoSave = false;
+  nlp.addLanguage('en');
+
+  // Load corpus data from a JSON file
+  const corpusData = await fs.readFile('../corpus.json', 'utf8');
+  const corpus = JSON.parse(corpusData);
+
+  // Add the corpus data to the manager
+  nlp.addCorpus(corpus);
+
+  // Train the model
+  await nlp.train();
+  //nlp.save();
+
+  const minified = true;
+  const nlpModel = nlp.export(minified);
+
+  // Save the model to a JSON file
+  await fs.writeFile('nlpModel.json', nlpModel, 'utf8');
+
+  // Process the input text
+  const response = await nlp.process('en', "Hakifred");
+  if (response.intent == 'None') {
+   // console.log('NO_ANSWER');
+  } else {
+    console.log(`ANSWER: This issue looks similar to a closed issue: ${response.answer}`);
+  }
+})();
